26230df6da3245c046c459a646733b6e0a1e52c1
Anirup Patnaik <anirup049@gmail.com>
2022-03-08 20:46:20 +0530

    Tabs Addition

--- src/modules/home/components/main/Main.jsx
+++ src/modules/home/components/main/Main.jsx
@@ -1,10 +1,19 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
 import { stories } from "../../../../utils/data/stories";
 import { authors } from "../../../../utils/data/authors";
 import StoryCard from "../../../../components/storyCard/StoryCard";
 import styles from "./Main.module.scss";
 
 const Main = () => {
+  const [value, setValue] = useState("1");
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   let postData = stories.map((story) => {
     let authorData = authors.filter((author) => author.id === story.authorId);
     return { story, ...authorData[0] };
@@ -13,7 +22,20 @@ const Main = () => {
   let storyLists = postData.map((story) => {
     return <StoryCard story={story} />;
   });
-  return <div className={styles.main}>{storyLists}</div>;
+  return (
+    <div className={styles.main}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Following" value="1" />
+            <Tab label="Recommended" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">{storyLists[0]}</TabPanel>
+        <TabPanel value="2">{storyLists}</TabPanel>
+      </TabContext>
+    </div>
+  );
 };
 
 export default Main;
